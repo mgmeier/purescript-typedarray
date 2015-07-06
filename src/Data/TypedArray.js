90e60b34163a7e3f4d291270ec55946d17736cf7
jutaro <jnf@arcor.de>
2015-07-06 21:44:10 +0200

    0.7 meltdown changes.

---
+++ src/Data/TypedArray.js
@@ -0,0 +1,70 @@
+/* global exports */
+
+// module Data.TypedArray
+
+(function () {
+    "use strict";
+
+    // conversion from and to typed arrays
+    exports.asInt8Array = function(array)
+        {var res = new Int8Array(array);
+         return res;};
+
+    exports.asUint8Array = function (array)
+        {var res = new Uint8Array(array);
+         return res;};
+
+    exports.asUint8ClampedArray = function (array)
+        {var res = new Uint8ClampedArray(array);
+         return res;};
+
+    exports.asInt16Array = function (array)
+        {var res = new Int16Array (array);
+         return res;};
+
+    exports.asUint16Array = function (array)
+        {var res = new Uint16Array (array);
+         return res;};
+
+    exports.asInt32Array = function (array)
+        {var res = new Int32Array (array);
+         return res;};
+
+    exports.asUint32Array = function (array)
+        {var res = new Uint32Array (array);
+         return res;};
+
+    exports.asFloat32Array = function (array)
+        {var res = new Float32Array (array);
+         return res;};
+
+    exports.asFloat64Array = function (array)
+        {var res = new Float64Array (array);
+         return res;};
+
+    exports.asArray = function (arrayBuffer)
+        {var res = Array.prototype.slice.call(arrayBuffer);
+         return res;};
+
+    exports.length = function (arrayBuffer)
+        {return arrayBuffer.length;};
+
+    exports.byteLength = function (arrayBuffer)
+        {return arrayBuffer.byteLength;};
+
+    exports.insertAt = function (index) {
+        return function (a) {
+          return function (l) {
+            var l1 = l.slice();
+            l1.splice(index, 0, a);
+            return l1;
+          };
+        };
+    };
+
+    exports.unsafeIndex = function (xs) {
+        return function(n) {
+          return xs[n];
+        };
+    };
+})();
--- src/Data/TypedArray.purs
+++ src/Data/TypedArray.purs
@@ -16,104 +16,42 @@
 module Data.TypedArray where
 
 import Data.ArrayBuffer.Types
-
 import Data.Maybe
+import Prelude
 
 -- * conversion from and to typed arrays
-foreign import asInt8Array
-  """function asInt8Array(array)
-    {var res = new Int8Array (array);
-     return res;};"""
-    :: [Number] -> Int8Array
+foreign import asInt8Array :: Array Number -> Int8Array
 
-foreign import asUint8Array
-  """function asUint8Array(array)
-    {var res = new Uint8Array (array);
-     return res;};"""
-    :: [Number] -> Uint8Array
+foreign import asUint8Array :: Array Number -> Uint8Array
 
-foreign import asUint8ClampedArray
-  """function asUint8ClampedArray(array)
-    {var res = new Uint8ClampedArray (array);
-     return res;};"""
-    :: [Number] -> Uint8ClampedArray
+foreign import asUint8ClampedArray :: Array Number -> Uint8ClampedArray
 
-foreign import asInt16Array
-  """function asInt16Array(array)
-    {var res = new Int16Array (array);
-     return res;};"""
-    :: [Number] -> Int16Array
+foreign import asInt16Array :: Array Number -> Int16Array
 
-foreign import asUint16Array
-  """function asUint16Array(array)
-    {var res = new Uint16Array (array);
-     return res;};"""
-    :: [Number] -> Uint16Array
+foreign import asUint16Array :: Array Number -> Uint16Array
 
-foreign import asInt32Array
-  """function asInt32Array(array)
-    {var res = new Int32Array (array);
-     return res;};"""
-    :: [Number] -> Int32Array
+foreign import asInt32Array :: Array Number -> Int32Array
 
-foreign import asUint32Array
-  """function asUint32Array(array)
-    {var res = new Uint32Array (array);
-     return res;};"""
-    :: [Number] -> Uint32Array
+foreign import asUint32Array :: Array Number -> Uint32Array
 
-foreign import asFloat32Array
-  """function asFloat32Array(array)
-    {var res = new Float32Array (array);
-     return res;};"""
-    :: [Number] -> Float32Array
+foreign import asFloat32Array :: Array Number -> Float32Array
 
-foreign import asFloat64Array
-  """function asFloat64Array(array)
-    {var res = new Float64Array (array);
-     return res;};"""
-    :: [Number] -> Float64Array
+foreign import asFloat64Array :: Array Number -> Float64Array
 
-foreign import asArray
-  """function asArray(arrayBuffer)
-    {var res = Array.prototype.slice.call(arrayBuffer);;
-     return res;};"""
-    :: forall a. ArrayView a -> [Number]
+foreign import asArray :: forall a. ArrayView a -> Array Number
 
-foreign import length
-  """function length(arrayBuffer)
-    {return arrayBuffer.length;};"""
-    :: forall a. ArrayView a -> Number
+foreign import length :: forall a. ArrayView a -> Int
 
-foreign import byteLength
-  """function byteLength(arrayBuffer)
-    {return arrayBuffer.byteLength;};"""
-    :: forall a. ArrayView a -> Number
+foreign import byteLength :: forall a. ArrayView a -> Number
 
 infixl 8 !!
 
-foreign import insertAt
-  "function insertAt (index) {\
-  \  return function (a) {\
-  \    return function (l) {\
-  \      var l1 = l.slice();\
-  \      l1.splice(index, 0, a);\
-  \      return l1;\
-  \    }; \
-  \  };\
-  \}":: forall a. Number -> a -> ArrayView a -> ArrayView a
+foreign import insertAt :: forall a. Number -> a -> ArrayView a -> ArrayView a
 
-(!!) :: forall a. ArrayView a -> Number -> Maybe Number
+(!!) :: forall a. ArrayView a -> Int -> Maybe Number
 (!!) xs n =
-  if n < 0 || n >= (length xs) || isInt n
+  if n < 0 || n >= (length xs)
     then Nothing
     else Just (xs `unsafeIndex` n)
-  where
-  isInt n = n /= complement (complement n)
 
-foreign import unsafeIndex
-  "function unsafeIndex(xs) {\
-  \  return function(n) {\
-  \    return xs[n];\
-  \  };\
-  \}" :: forall a. ArrayView a -> Number -> Number
+foreign import unsafeIndex :: forall a. ArrayView a -> Int -> Number
